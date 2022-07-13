f6cc9ca7203ecac3c9be54e68d3f71a9e946c97d
gomi_ningen <gomi2ngen@gmail.com>
2022-07-14 02:16:48 +0900

    非ログイン時の非公開ページリンクを無効化

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react'
 import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom'
+import { useAuthContext } from '../../context/AuthContext'
 
 export const Link = React.forwardRef<any, Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }>(
   (props, ref) => {
-    const { href, ...other } = props
-    return <RouterLink ref={ref} to={href} {...other} style={{ textDecoration: 'none' }} />
+    const { isAuthenticated } = useAuthContext()
+    const { href, children, ...other } = props
+    const isPrivate = href.toString() === '/private' || href.toString().startsWith('/private/')
+    const disabled = isPrivate && !isAuthenticated
+    if (disabled) {
+      return (
+        <a href={href.toString()} ref={ref} {...other} style={{ textDecoration: 'none', pointerEvents: 'none' }}>
+          {children}
+        </a>
+      )
+    }
+    return (
+      <RouterLink ref={ref} to={href} {...other} style={{ textDecoration: 'none' }}>
+        {children}
+      </RouterLink>
+    )
   }
 )
